bbe45ea1ecdcfc868fb4ceaef42142f6a8a663f0
Kevin Coffey <kevdougful@gmail.com>
2024-03-11 16:42:18 -0500

    Remove Junk
    Copilot prolly put this in there

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,6 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
 
-  const onSubmit = (params: any) => {
-    console.log(params);
-    return new Promise((resolve) => {
-      setTimeout(resolve, 1000);
-    });
-  };
-
   return (
     <>
       <Heading size="xl">What's Happening at ABC</Heading>
