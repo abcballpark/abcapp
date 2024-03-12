0db6fb814c550532b56766ea53a8cc45a31a4e5e
Kevin Coffey <kevdougful@gmail.com>
2024-03-12 16:29:29 -0500

    Testing Preview Deployment

--- app/baseball/page.tsx
+++ app/baseball/page.tsx
@@ -2,7 +2,7 @@ export default function Baseball() {
   return (
     <div>
       <h1>Baseball</h1>
-      <p>Welcome to the baseball page!</p>
+      <p>Welcome to the Baseball page!</p>
     </div>
   );
 }
